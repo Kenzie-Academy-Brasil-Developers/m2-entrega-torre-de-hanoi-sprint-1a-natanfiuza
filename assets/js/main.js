52a93b59d3cb5954c0c9870bc8d1551e29d0a781
Rodrigo Tavares <rodrigotvrs70@hotmail.com>
2021-11-29 16:11:25 -0300

    estrutura basica do jogo em arrays

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,31 @@
-/**
- * @package Torre de Hanoi
- * @since 2021-11-29
- * @description Jogo Torre de Hanoi
- * 
- */
+const arr = {
+  start: [0, 1, 2, 3],
+  buffer: new Array(),
+  finish: new Array()
+}
 
+// function movePiece(arrName){
+
+// }
+
+function createDOM(){
+  const target = document.querySelector("tbody")
+  const tr = document.createElement("tr")
+
+  const templateRow = `
+    <td>1</td>
+    <td>2</td>
+    <td>3</td>
+  `
+
+  tr.innerHTML = templateRow
+
+  target.appendChild(tr)
+}
+
+// function init(){
+//   // createDOM()
+
+// }
+
+// init()
