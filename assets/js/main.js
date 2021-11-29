634fa478868a44985fc4a2e3aaa15eddc80b84af
NatanFiuza <natan.fiuza@gmail.com>
2021-11-29 19:58:09 -0300

    Alterações em createDOM

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,60 +1,44 @@
 const arr = {
-  start: [0, 1, 2, 3],
-  buffer:new Array(),
+  start: [0, 1, 2 ],
+  buffer: [3],
   finish: new Array(),
-  clicked: new Array()
-}
+  clicked: new Array(),
+};
 
-function movePiece(currentArr, targetArr){
-  const currentArrLastIndex = currentArr.length - 1
-  const targetArrLastIndex = targetArr.length - 1
-  
-  const current = currentArr[currentArrLastIndex]
-  const target = targetArr[targetArrLastIndex]
+function movePiece(currentArr, targetArr) {
+  const currentArrLastIndex = currentArr.length - 1;
+  const targetArrLastIndex = targetArr.length - 1;
+
+  const current = currentArr[currentArrLastIndex];
+  const target = targetArr[targetArrLastIndex];
 
   const isMovementValid = () => {
-    if(currentArr === targetArr) return false
-    if(currentArrLastIndex === -1) return false
-    if(targetArrLastIndex === -1) return true
-    if(target < current) return true
+    if (currentArr === targetArr) return false;
+    if (currentArrLastIndex === -1) return false;
+    if (targetArrLastIndex === -1) return true;
+    if (target < current) return true;
 
-    return false
+    return false;
+  };
+
+  if (isMovementValid()) {
+    const value = currentArr.pop();
+    targetArr.push(value);
+
+    return true;
   }
 
-  if(isMovementValid()){
-    const value = currentArr.pop()
-    targetArr.push(value)
-    
-    return true
-  }
-
-  return false
+  return false;
 }
 
-function handleInteraction(name){  
-  arr.clicked.push(name)
+function handleInteraction(name) {
+  arr.clicked.push(name);
 
-  if(arr.clicked.length > 1){
-    movePiece(arr[arr.clicked[0]], arr[arr.clicked[1]])
-    arr.clicked = new Array()
+  if (arr.clicked.length > 1) {
+    movePiece(arr[arr.clicked[0]], arr[arr.clicked[1]]);
+    arr.clicked = new Array();
   }
 }
-
-// function createDOM(){
-//   const target = document.querySelector("tbody")
-//   const tr = document.createElement("tr")
-
-//   const templateRow = `
-//     <td>1</td>
-//     <td>2</td>
-//     <td>3</td>
-//   `
-
-//   tr.innerHTML = templateRow
-
-//   target.appendChild(tr)
-// }
-
 
 function createDOM() {
   const table = document.getElementById("towers");
@@ -63,32 +47,20 @@ function createDOM() {
 
   let tr = table.insertRow(-1); // Insere uma linha
 
-  let td = tr.insertCell(-1); // Insere uma celula ao final e retorna seu identificador
-  td.addEventListener("click", function (e) {
-    // handleInteraction("start")
-    console.log("start");
-    destakPiece("start");
-  });
-  td.classList.add(`pin1`);
-  td.id = "pin1";
-
-  td = tr.insertCell(-1); // Insere uma celula ao final e retorna seu identificador
-  td.addEventListener("click", function (e) {
-    // handleInteraction("buffer")
-    console.log("buffer");
-    destakPiece("buffer");
-  });
-  td.classList.add(`pin2`);
-  td.id = "pin2";
-
-  td = tr.insertCell(-1); // Insere uma celula ao final e retorna seu identificador
-  td.addEventListener("click", function (e) {
-    // handleInteraction("finish")
-    console.log("finish");
-    destakPiece("finish");
-  });
-  td.classList.add(`pin3`);
-  td.id = "pin3";
+  for (let i = 1; i < 4; i++) {
+    let td = tr.insertCell(-1); // Insere uma celula ao final e retorna seu identificador
+  
+    td.classList.add(`pin${i}`);
+    td.id = `pin${i}`;
+    td.addEventListener("click", function (e) {
+        handleInteraction(i==1?"start":i==2?"buffer":"finish");
+        console.log(arr)
+        createPiece();
+        console.log(i==1?"start":i==2?"buffer":"finish");
+        destakPiece("start");
+      });
+      console.log(td)           
+  } 
 
   createPiece();
 }
@@ -97,7 +69,9 @@ function createPiece() {
   let pin1 = document.getElementById("pin1");
   let pin2 = document.getElementById("pin2");
   let pin3 = document.getElementById("pin3");
-
+  pin1.innerHTML = ""
+  pin2.innerHTML = ""
+  pin3.innerHTML = ""
   arr.start.forEach(function (e) {
     let piece = document.createElement("div");
     piece.id = `piece_${e}`;
@@ -119,19 +93,22 @@ function createPiece() {
 }
 
 function destakPiece(pin) {
-    //document.getElementById('piece0').classList.remove("destak")
-   // document.getElementById('piece1').classList.remove("destak")
-   // document.getElementById('piece2').classList.remove("destak")
-   // document.getElementById('piece3').classList.remove("destak")
-    switch (pin) {
-        case "start":
-            let piece = document.getElementById(`piece${arr.start[arr.start.length - 1]}`); 
-            piece.classList.add("destak");
-            break;
-    
-        default:
-            break;
-    }
+  //document.getElementById('piece0').classList.remove("destak")
+  // document.getElementById('piece1').classList.remove("destak")
+  // document.getElementById('piece2').classList.remove("destak")
+  // document.getElementById('piece3').classList.remove("destak")
+  switch (pin) {
+    case "start":
+      let piece = document.getElementById(
+        `piece${arr.start[arr.start.length - 1]}`
+      );
+      
+    //  piece.classList.add("destak");
+      break;
+
+    default:
+      break;
+  }
 }
 // function init(){
 //   // createDOM()
