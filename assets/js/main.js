222d99453e49c26062e95b5e3578339aeba16b96
Rodrigo Tavares <rodrigotvrs70@hotmail.com>
2021-11-29 17:20:21 -0300

    movePiece funçao criada

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,62 @@
 const arr = {
   start: [0, 1, 2, 3],
-  buffer: new Array(),
+  buffer:new Array(),
   finish: new Array()
 }
 
-// function movePiece(arrName){
+let clicked = new Array()
 
+function movePiece(currentArr, targetArr){
+  const currentArrLastIndex = currentArr.length - 1
+  const targetArrLastIndex = targetArr.length - 1
+  
+  const current = currentArr[currentArrLastIndex]
+  const target = targetArr[targetArrLastIndex]
+
+  const isMovementValid = () => {
+    if(currentArrLastIndex === -1) return false
+    if(targetArrLastIndex === -1) return true
+    if(target > current) return true
+
+    return false
+  }
+
+  if(isMovementValid()){
+    const value = currentArr.pop()
+    targetArr.push(value)
+
+    return true
+  }
+
+  return false
+}
+
+// function handleClick(e){
+//   const name = "oi"
+  
+//   if(clicked.length == 2){
+//     movePiece(arr[0],arr)
+//     clicked = new Array()
+//   }
+//   else{
+
+//   }
 // }
 
-function createDOM(){
-  const target = document.querySelector("tbody")
-  const tr = document.createElement("tr")
+// function createDOM(){
+//   const target = document.querySelector("tbody")
+//   const tr = document.createElement("tr")
 
-  const templateRow = `
-    <td>1</td>
-    <td>2</td>
-    <td>3</td>
-  `
+//   const templateRow = `
+//     <td>1</td>
+//     <td>2</td>
+//     <td>3</td>
+//   `
 
-  tr.innerHTML = templateRow
+//   tr.innerHTML = templateRow
 
-  target.appendChild(tr)
-}
+//   target.appendChild(tr)
+// }
 
 // function init(){
 //   // createDOM()
@@ -29,3 +64,6 @@ function createDOM(){
 // }
 
 // init()
+
+console.log(movePiece(arr.start, arr.buffer))
+console.log(arr)
